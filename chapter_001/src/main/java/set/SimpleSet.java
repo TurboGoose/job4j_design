6db4eec0119c4740fc3465c0bde8e0fe2ad04ec5
Ilya <antonpuckich@gmail.com>
2020-09-18 21:06:42 +0300

    1. Реализовать коллекцию Set на массиве [#352122]

--- chapter_001/src/main/java/set/SimpleSet.java
+++ chapter_001/src/main/java/set/SimpleSet.java
@@ -2,14 +2,24 @@ package set;
 import list.SimpleArray;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<T> implements Iterable<T> {
     private final SimpleArray<T> array = new SimpleArray<>();
 
     public void add(T value) {
-        if (!array.contains(value)) {
+        if (!contains(value)) {
             array.add(value);
         }
+    }
+
+    private boolean contains(T value) {
+        for (T element : array) {
+            if (Objects.equals(element, value)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
