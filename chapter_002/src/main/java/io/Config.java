8aabd1eb51c447de0a213c62ad44d23144504f81
Ilya <antonpuckich@gmail.com>
2020-09-28 15:41:16 +0300

    1. Читаем файл конфигурации [#352151]

---
+++ chapter_002/src/main/java/io/Config.java
@@ -0,0 +1,45 @@
+package io;
+
+import java.io.*;
+import java.util.*;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            in.lines().forEach(line -> {
+                if (line.isEmpty() || line.startsWith("//")) {
+                    return;
+                }
+                String[] keyValue = line.split("=");
+                if (keyValue.length != 2) {
+                    return;
+                }
+                values.put(keyValue[0], keyValue[1]);
+            });
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+}
---
+++ chapter_002/src/test/java/io/ConfigTest.java
@@ -0,0 +1,55 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class ConfigTest {
+    @Test
+    public void emptyPropertyFile() {
+        Config conf = new Config("../files/read/testProperties/empty.txt");
+        conf.load();
+        assertThat(conf.value(""), is(nullValue()));
+    }
+
+    @Test
+    public void propertyFileWithoutCommentsAndEmptyLines() {
+        Config conf = new Config("../files/read/testProperties/withoutCommentsAndEmptyLines.txt");
+        conf.load();
+        assertThat(conf.value("property1"), is("1"));
+        assertThat(conf.value("property2"), is("2"));
+        assertThat(conf.value("property3"), is("3"));
+    }
+
+    @Test
+    public void propertyFileWithEmptyLines() {
+        Config conf = new Config("../files/read/testProperties/withEmptyLines.txt");
+        conf.load();
+        assertThat(conf.value("property1"), is("1"));
+        assertThat(conf.value("property2"), is("2"));
+        assertThat(conf.value("property3"), is("3"));
+        assertThat(conf.value(""), is(nullValue()));
+    }
+
+    @Test
+    public void propertyFileWithComments() {
+        Config conf = new Config("../files/read/testProperties/withComments.txt");
+        conf.load();
+        assertThat(conf.value("property1"), is("1"));
+        assertThat(conf.value("property2"), is("2"));
+        assertThat(conf.value("property3"), is("3"));
+        assertThat(conf.value("//comment"), is(nullValue()));
+    }
+
+    @Test
+    public void propertyFileWithCommentsAndEmptyLines() {
+        Config conf = new Config("../files/read/testProperties/withCommentsAndEmptyLines.txt");
+        conf.load();
+        assertThat(conf.value("property1"), is("1"));
+        assertThat(conf.value("property2"), is("2"));
+        assertThat(conf.value("property3"), is("3"));
+        assertThat(conf.value(""), is(nullValue()));
+        assertThat(conf.value("//comment"), is(nullValue()));
+    }
+}
