d422aa62c9c000036af98bdc360c407a51123f27
Ilya <antonpuckich@gmail.com>
2020-09-29 23:04:28 +0300

    1. Читаем файл конфигурации [#352151]

--- chapter_002/src/test/java/io/ConfigTest.java
+++ chapter_002/src/test/java/io/ConfigTest.java
@@ -1,54 +1,90 @@
 package io;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 class ConfigTest {
+    @TempDir
+    File tempDir;
+
     @Test
     public void emptyPropertyFile() {
-        Config conf = new Config("../files/testss/ConfigTest/empty.txt");
+        File source = new File(tempDir, "test.txt");
+        Config conf = new Config(source.getAbsolutePath());
         conf.load();
         assertThat(conf.value(""), is(nullValue()));
     }
 
     @Test
-    public void propertyFileWithoutCommentsAndEmptyLines() {
-        Config conf = new Config("../files/tests/ConfigTest/withoutCommentsAndEmptyLines.txt");
+    public void propertyFileWithoutCommentsAndEmptyLines() throws IOException {
+        String inputData =
+                "property1=1\n" +
+                "property2=2";
+        File source = new File(tempDir, "test.txt");
+        try (FileWriter sourceFile = new FileWriter(source)) {
+            sourceFile.write(inputData);
+        }
+        Config conf = new Config(source.getAbsolutePath());
         conf.load();
         assertThat(conf.value("property1"), is("1"));
         assertThat(conf.value("property2"), is("2"));
-        assertThat(conf.value("property3"), is("3"));
     }
 
     @Test
-    public void propertyFileWithEmptyLines() {
-        Config conf = new Config("../files/tests/ConfigTest/withEmptyLines.txt");
+    public void propertyFileWithEmptyLines() throws IOException {
+        String inputData =
+                "property1=1\n\n\n" +
+                "property2=2\n";
+        File source = new File(tempDir, "test.txt");
+        try (FileWriter sourceFile = new FileWriter(source)) {
+            sourceFile.write(inputData);
+        }
+        Config conf = new Config(source.getAbsolutePath());
         conf.load();
         assertThat(conf.value("property1"), is("1"));
         assertThat(conf.value("property2"), is("2"));
-        assertThat(conf.value("property3"), is("3"));
         assertThat(conf.value(""), is(nullValue()));
     }
 
     @Test
-    public void propertyFileWithComments() {
-        Config conf = new Config("../files/tests/ConfigTest/withComments.txt");
+    public void propertyFileWithComments() throws IOException {
+        String inputData =
+                "property1=1\n" +
+                "//comment\n" +
+                "property2=2";
+        File source = new File(tempDir, "test.txt");
+        try (FileWriter sourceFile = new FileWriter(source)) {
+            sourceFile.write(inputData);
+        }
+        Config conf = new Config(source.getAbsolutePath());
         conf.load();
         assertThat(conf.value("property1"), is("1"));
         assertThat(conf.value("property2"), is("2"));
-        assertThat(conf.value("property3"), is("3"));
         assertThat(conf.value("//comment"), is(nullValue()));
     }
 
     @Test
-    public void propertyFileWithCommentsAndEmptyLines() {
-        Config conf = new Config("../files/tests/ConfigTest/withCommentsAndEmptyLines.txt");
+    public void propertyFileWithCommentsAndEmptyLines() throws IOException {
+        String inputData =
+                "property1=1\n\n\n" +
+                "//comment\n\n" +
+                "property2=2\n" +
+                "//comment\n\n";
+        File source = new File(tempDir, "test.txt");
+        try (FileWriter sourceFile = new FileWriter(source)) {
+            sourceFile.write(inputData);
+        }
+        Config conf = new Config(source.getAbsolutePath());
         conf.load();
         assertThat(conf.value("property1"), is("1"));
         assertThat(conf.value("property2"), is("2"));
-        assertThat(conf.value("property3"), is("3"));
         assertThat(conf.value(""), is(nullValue()));
         assertThat(conf.value("//comment"), is(nullValue()));
     }
