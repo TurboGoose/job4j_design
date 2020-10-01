2928a1a74f806dd778dc8ffdc7899183802a0c83
Ilya <antonpuckich@gmail.com>
2020-10-01 12:38:43 +0300

    5. Валидация параметров запуска. [#352144]

---
+++ chapter_002/src/main/java/io/ArgsName.java
@@ -0,0 +1,32 @@
+package io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (values.containsKey(key)) {
+            return values.get(key);
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            String[] keyValue = arg.substring(1).split("=");
+            if (keyValue.length != 2) {
+                throw new IllegalArgumentException("Wrong format: " + arg);
+            }
+            values.putIfAbsent(keyValue[0], keyValue[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+}
---
+++ chapter_002/src/test/java/io/ArgsNameTest.java
@@ -0,0 +1,27 @@
+package io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArgsNameTest {
+    @Test
+    public void whenGetFirst() {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenGetFirstReorder() {
+        ArgsName jvm = ArgsName.of(new String[] {"-encoding=UTF-8", "-Xmx=512"});
+        assertThat(jvm.get("Xmx"), is("512"));
+    }
+
+    @Test
+    public void whenGetNotExist() {
+        ArgsName jvm = ArgsName.of(new String[] {});
+        assertThrows(IllegalArgumentException.class, () -> jvm.get("Xmx"));
+    }
+}
