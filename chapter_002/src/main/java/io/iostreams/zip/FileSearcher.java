1ec2d00b01bea1bb28a91fb3134887fad0376ba5
Ilya <antonpuckich@gmail.com>
2020-10-08 19:33:24 +0300

    5.2. Архивировать проект [#352153]

---
+++ chapter_002/src/main/java/io/iostreams/zip/FileSearcher.java
@@ -0,0 +1,37 @@
+package io.iostreams.zip;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+class FileSearcher extends SimpleFileVisitor<Path> {
+    private final Predicate<Path> predicate;
+    private final List<Path> paths = new ArrayList<>();
+
+    public FileSearcher(String extension) {
+        this.predicate = extension == null ? null : path -> path.toString().endsWith(extension);
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        if (predicate == null || !predicate.test(file)) {
+            paths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        paths.add(dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
--- chapter_002/src/main/java/io/iostreams/zip/Zip.java
+++ chapter_002/src/main/java/io/iostreams/zip/Zip.java
@@ -2,7 +2,6 @@ package io.iostreams.zip;
 
 import java.io.*;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -15,30 +14,21 @@ public class Zip {
         }
     }
 
-    public void packDirectory(Path sourceDir, Path targetFile) throws IOException {
+    public void packDirectory(Path sourceDir, Path targetFile)  throws IOException {
         packDirectory(sourceDir, targetFile, null);
     }
 
     public void packDirectory(Path sourceDir, Path targetFile, String toExclude) throws IOException {
+        FileSearcher searcher = new FileSearcher(toExclude);
+        Files.walkFileTree(sourceDir, searcher);
         try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(targetFile.toFile()))) {
-            Files.walkFileTree(sourceDir, new SimpleFileVisitor<>() {
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (toExclude == null || !file.getFileName().toString().endsWith(toExclude)) {
-                        zos.putNextEntry(new ZipEntry(sourceDir.relativize(file).toString()));
-                        Files.copy(file, zos);
-                        zos.closeEntry();
-                    }
-                    return FileVisitResult.CONTINUE;
+            for (Path src : searcher.getPaths()) {
+                zos.putNextEntry(new ZipEntry(sourceDir.relativize(src) + (Files.isDirectory(src) ? "/" : "")));
+                if (!Files.isDirectory(src)) {
+                    Files.copy(src, zos);
                 }
-
-                @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    zos.putNextEntry(new ZipEntry(sourceDir.relativize(dir).toString() + File.separator));
-                    zos.closeEntry();
-                    return FileVisitResult.CONTINUE;
-                }
-            });
+                zos.closeEntry();
+            }
         }
     }
 }
