6bf7e4eba1099ad43e44dc2d208bd6b134f08d2c
Ilya <antonpuckich@gmail.com>
2020-10-09 17:47:26 +0300

    Client-Server tutorial

--- chapter_002/src/main/java/io/socket/tutorial/Server.java
+++ chapter_002/src/main/java/io/socket/tutorial/Server.java
@@ -5,40 +5,18 @@ import java.net.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8000);
-        System.out.println("Server running...");
-
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            String line;
-            String clientNum;
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            clientSocket.getInputStream()));
-            System.out.println("Request:");
-            line = in.readLine();
-            clientNum = line.split(" ")[1].substring(1);
-            System.out.println(line);
-
-
-            BufferedWriter out = new BufferedWriter(
-                    new OutputStreamWriter(
-                            clientSocket.getOutputStream()));
-            String request = String.format("Client #%d is processed" + System.lineSeparator(), clientNum);
-            out.write(request);
-            out.flush();
-            System.out.println("Response: " + request);
-
-            in.close();
-            out.close();
-            clientSocket.close();
+        try (ServerSocket serverSocket = new ServerSocket(8000)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream())) {
+                    String line;
+                    while (!(line = in.readLine()).isEmpty()) {
+                        System.out.println(line);
+                    }
+                    out.println("*** Processed ***");
+                }
+            }
         }
-    }
-
-    public static String generateHttpRequest(String str) {
-        return String.join(System.lineSeparator(),
-                "HTTP/1.0 200 OK",
-                "Content-type: text/html",
-                "") + str;
     }
 }
