02a4c92da4e57b6a28ab08c4985855817a63b278
Ilya <antonpuckich@gmail.com>
2020-10-14 23:44:58 +0300

    5. Преобразование JSON в POJO. JsonObject [#352161]

---
+++ chapter_002/src/main/java/io/serialization/json/cyclic/A.java
@@ -0,0 +1,27 @@
+package io.serialization.json.cyclic;
+
+import org.json.JSONPropertyIgnore;
+
+public class A {
+    private B b;
+
+    @JSONPropertyIgnore
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+
+    @Override
+    public String toString() {
+        return "A{" +
+                "b=" + b +
+                '}';
+    }
+
+    public String getHello() {
+        return "Hello";
+    }
+}
--- chapter_002/src/main/java/io/serialization/json/person/Main.java
+++ chapter_002/src/main/java/io/serialization/json/person/Main.java
@@ -2,16 +2,29 @@ package io.serialization.json.person;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 public class Main {
-    public static void main(String[] args) {
-        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+    private final static Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
 
+
+    public static void main(String[] args) {
+        testGson();
+        testJson();
+    }
+
+    public static void testGson() {
         final Gson gson = new GsonBuilder().create();
         String personJson = gson.toJson(person);
         System.out.println(personJson);
 
-        final Person personMod = gson.fromJson(personJson, Person.class);
+        Person personMod = gson.fromJson(personJson, Person.class);
         System.out.println(personMod);
+    }
+
+    public static void testJson() {
+        JSONObject jsonObject = new JSONObject(person);
+        String personJson = jsonObject.toString();
+        System.out.println(personJson);
     }
 }
