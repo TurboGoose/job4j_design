5e23601a3162ad9f8b4fb37b5284bd4271e89ce3
Ilya <antonpuckich@gmail.com>
2020-10-24 22:40:59 +0300

    Поиск файлов по критерию [#352164] Argument parser is written

---
+++ chapter_002/src/main/java/io/searchfiles/ArgumentParser.java
@@ -0,0 +1,99 @@
+package io.searchfiles;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArgumentParser {
+    public enum SearchOption {FULL_NAME, REGEX, MASK}
+
+    private SearchOption searchOption;
+    private String searchDirectory;
+    private String outputFile;
+    private String fileName;
+    private boolean valid;
+
+    public ArgumentParser(String[] args) {
+        valid = parse(args);
+    }
+
+    private boolean parse(String[] args) {
+        Iterator<String> it = Arrays.stream(args).iterator();
+        while (it.hasNext()) {
+            String arg = it.next();
+            if (List.of("-d", "-n", "-o").contains(arg)) {
+                if (!it.hasNext()) {
+                    return false;
+                }
+                switch (arg) {
+                    case "-d" -> {
+                        if (searchDirectory != null) {
+                            return false;
+                        }
+                        searchDirectory = it.next();
+                    }
+                    case "-n" -> {
+                        if (fileName != null) {
+                            return false;
+                        }
+                        fileName = it.next();
+                    }
+                    case "-o" -> {
+                        if (outputFile != null) {
+                            return false;
+                        }
+                        outputFile = it.next();
+                    }
+                }
+            }
+            else if (List.of("-m", "-r", "-f").contains(arg)) {
+                if (searchOption != null) {
+                    return false;
+                }
+                searchOption = switch (arg) {
+                    case "-m" -> SearchOption.MASK;
+                    case "-r" -> SearchOption.REGEX;
+                    case "-f" -> SearchOption.FULL_NAME;
+                    default -> null;
+                };
+            }
+            else {
+                return false;
+            }
+        }
+        return searchDirectory != null && outputFile != null &&
+                fileName != null && searchOption != null;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public SearchOption getSearchOption() {
+        if (!valid) {
+            throw new UnsupportedOperationException();
+        }
+        return searchOption;
+    }
+
+    public String getSearchDirectory() {
+        if (!valid) {
+            throw new UnsupportedOperationException();
+        }
+        return searchDirectory;
+    }
+
+    public String getOutputFile() {
+        if (!valid) {
+            throw new UnsupportedOperationException();
+        }
+        return outputFile;
+    }
+
+    public String getFileName() {
+        if (!valid) {
+            throw new UnsupportedOperationException();
+        }
+        return fileName;
+    }
+}
---
+++ chapter_002/src/test/java/io/searchfiles/ArgumentParserTest.java
@@ -0,0 +1,54 @@
+package io.searchfiles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArgumentParserTest {
+
+    @Test
+    public void normalUse() {
+        ArgumentParser parser = new ArgumentParser("-d c:/ -n *.txt -m -o log.txt".split(" "));
+        assertThat(parser.isValid(), is(true));
+        assertThat(parser.getFileName(), is("*.txt"));
+        assertThat(parser.getOutputFile(), is("log.txt"));
+        assertThat(parser.getSearchDirectory(), is("c:/"));
+        assertThat(parser.getSearchOption(), is(ArgumentParser.SearchOption.MASK));
+    }
+
+    @Test
+    public void parseEmptyArguments() {
+        ArgumentParser parser = new ArgumentParser(new String[0]);
+        assertThat(parser.isValid(), is(false));
+        assertThrows(UnsupportedOperationException.class, parser::getFileName);
+        assertThrows(UnsupportedOperationException.class, parser::getOutputFile);
+        assertThrows(UnsupportedOperationException.class, parser::getSearchDirectory);
+        assertThrows(UnsupportedOperationException.class, parser::getSearchOption);
+    }
+
+    @Test
+    public void parseUnexpectedArguments() {
+        ArgumentParser parser = new ArgumentParser("-u -d c:/ -n *.txt -m -o log.txt".split(" "));
+        assertThat(parser.isValid(), is(false));
+    }
+
+    @Test
+    public void parseLessArgumentsThanRequired() {
+        ArgumentParser parser = new ArgumentParser("-n *.txt -o log.txt".split(" "));
+        assertThat(parser.isValid(), is(false));
+    }
+
+    @Test
+    public void parseDuplicateArguments() {
+        ArgumentParser parser = new ArgumentParser("-d c:/ -d d:/ -n *.txt -m -r -o log.txt".split(" "));
+        assertThat(parser.isValid(), is(false));
+    }
+
+    @Test
+    public void parseArgumentsWithEmptyParameterInTheEnd() {
+        ArgumentParser parser = new ArgumentParser("-d c:/ -d d:/ -n *.txt -m -r -o".split(" "));
+        assertThat(parser.isValid(), is(false));
+    }
+}
